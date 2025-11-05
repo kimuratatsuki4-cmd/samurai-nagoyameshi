34d2d55edd95df1b4fe584441766e7ab92c36e94
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-11-05 14:49:19 +0900

    add mailfunction

--- src/main/java/com/example/nagoyameshi/service/UserService.java
+++ src/main/java/com/example/nagoyameshi/service/UserService.java
@@ -38,7 +38,7 @@ public class UserService {
 		user.setEmail(signupForm.getEmail());
 		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		user.setRole(role);
-		user.setEnabled(true);
+		user.setEnabled(false);
 
 		if (!signupForm.getBirthday().isEmpty()) {
 			user.setBirthday(LocalDate.parse(signupForm.getBirthday(), DateTimeFormatter.ofPattern("yyyyMMdd")));
@@ -63,5 +63,11 @@ public class UserService {
 
 	public boolean isSamePassword(String password, String passwordConfirmation) {
 		return password.equals(passwordConfirmation);
+	}
+	
+	@Transactional
+	public void enableUser(User user) {
+		user.setEnabled(true);
+		userRepository.save(user);
 	}
 }
