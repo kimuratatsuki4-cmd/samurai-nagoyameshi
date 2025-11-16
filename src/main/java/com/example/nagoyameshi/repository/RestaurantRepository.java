ca6c0873465dab468ab5d097c1c14e5d651602ca
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-11-16 16:59:17 +0900

    add indexrestaurant

--- src/main/java/com/example/nagoyameshi/repository/RestaurantRepository.java
+++ src/main/java/com/example/nagoyameshi/repository/RestaurantRepository.java
@@ -3,14 +3,63 @@ package com.example.nagoyameshi.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.nagoyameshi.entity.Restaurant;
 
-
 @Repository
-public interface RestaurantRepository  extends JpaRepository<Restaurant, Integer>{
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 	public Page<Restaurant> findByNameLike(String name, Pageable pageable);
+
 	public Restaurant findFirstByOrderByIdDesc();
+
 	public Page<Restaurant> findAllRestaurantsByOrderByCreatedAtDesc(Pageable pageable);
+
+	public Page<Restaurant> findAllByOrderByLowestPriceAsc(Pageable pageable);
+
+	@Query("SELECT DISTINCT r FROM Restaurant r " +
+			"LEFT JOIN r.categoriesRestaurants cr " +
+			"WHERE r.name LIKE %:name% " +
+			"OR r.address LIKE %:address% " +
+			"OR cr.category.name LIKE %:categoryName% " +
+			"ORDER BY r.lowestPrice ASC")
+	public Page<Restaurant> findByNameLikeOrAddressLikeOrCategoryNameLikeOrderByCreatedAtDesc(
+			@Param("name") String nameKeyword,
+			@Param("address") String addressKeyword,
+			@Param("categoryName") String categoryNameKeyword,
+			Pageable pageable);
+	
+	@Query("SELECT DISTINCT r FROM Restaurant r " +
+			"LEFT JOIN r.categoriesRestaurants cr " +
+			"WHERE r.name LIKE %:name% " +
+			"OR r.address LIKE %:address% " +
+			"OR cr.category.name LIKE %:categoryName% " +
+			"ORDER BY r.lowestPrice DESC")
+	public Page<Restaurant> findRestaurantsByNameLikeOrAddressLikeOrCategoryNameLikeOrderByLowestPriceAsc(
+			@Param("name") String nameKeyword,
+			@Param("address") String addressKeyword,
+			@Param("categoryName") String categoryNameKeyword,
+			Pageable pageable);
+
+	// 指定されたidのカテゴリが設定された店舗を作成日時が新しい順に並べ替え、ページングされた状態で取得する
+	@Query("SELECT r FROM Restaurant r " +
+			"INNER JOIN r.categoriesRestaurants cr " +
+			"WHERE cr.category.id = :categoryId " +
+			"ORDER BY r.createdAt DESC")
+	public Page<Restaurant> findByCategoryIdOrderByCreatedAtDesc(@Param("categoryId") Integer categoryId,
+			Pageable pageable);
+
+	// 指定されたidのカテゴリが設定された店舗を最低価格が安い順に並べ替え、ページングされた状態で取得する
+	@Query("SELECT r FROM Restaurant r " +
+			"INNER JOIN r.categoriesRestaurants cr " +
+			"WHERE cr.category.id = :categoryId " +
+			"ORDER BY r.lowestPrice ASC")
+	public Page<Restaurant> findByCategoryIdOrderByLowestPriceAsc(@Param("categoryId") Integer categoryId,
+			Pageable pageable);
+
+	public Page<Restaurant> findByLowestPriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
+
+	public Page<Restaurant> findByLowestPriceLessThanEqualOrderByLowestPriceAsc(Integer price, Pageable pageable);
 }
